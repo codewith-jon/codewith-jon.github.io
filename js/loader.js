8f5d75c43c444ea9ba3fd4a73d22c305ecafd982
codewith-jon <78647762+codewith-jon@users.noreply.github.com>
2021-05-01 14:16:25 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,4 @@
-var mylo = setTimeout(loo, 3000);
+var mylo = setTimeout(loo, 5000);
 function loo() {
     document.getElementById("timeout").style.display = ("none");
     document.getElementById("log").style.display = ("block");
