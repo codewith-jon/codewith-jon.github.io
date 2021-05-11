9bd6b3f1495148e3015885590649b2a91fbfe552
codewith-jon <78647762+codewith-jon@users.noreply.github.com>
2021-05-11 22:28:52 +0300

    m

--- js/blur.js
+++
@@ -1,7 +0,0 @@
-var blur = setTimeout(blureffect, 30000);
-function blureffect() {
-    document.getElementById("body").style.filter = ("blur(0px");
-    document.getElementById("timeout").style.display = ("none");
-
-
-}
